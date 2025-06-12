d4a62d9e0eb5495c0933ad876028f2755f59170d
Ilya Katassonov <ilya.katassonov@gmail.com>
2025-06-12 20:31:54 +0100

    Removed extra markup from ConfigSkeleton

--- src/renderer/src/domains/Config/ConfigSkeleton.tsx
+++ src/renderer/src/domains/Config/ConfigSkeleton.tsx
@@ -1,32 +1,24 @@
 import { FC } from 'react'
-import { Box, Divider, Skeleton, Stack, Typography } from '@mui/material'
+import { Skeleton, Stack } from '@mui/material'
 
 export const ConfigSkeleton: FC = () => {
   return (
-    <Box p={4}>
-      <Typography variant="h5" gutterBottom>
-        Config
-      </Typography>
+    <Stack spacing={3}>
+      {/* Folder Inputs */}
+      <Skeleton variant="rounded" height={56} />
+      <Skeleton variant="rounded" height={56} />
 
-      <Divider sx={{ mb: 3 }} />
+      {/* Max Threads input */}
+      <Skeleton variant="rounded" height={56} />
 
-      <Stack spacing={3}>
-        {/* Folder Inputs */}
-        <Skeleton variant="rounded" height={56} />
-        <Skeleton variant="rounded" height={56} />
+      {/* Checkboxes */}
+      <Skeleton variant="text" width={240} height={42} />
+      <Skeleton variant="text" width={280} height={42} />
+      <Skeleton variant="text" width={200} height={42} />
+      <Skeleton variant="text" width={160} height={42} />
 
-        {/* Max Threads input */}
-        <Skeleton variant="rounded" height={56} />
-
-        {/* Checkboxes */}
-        <Skeleton variant="text" width={240} height={42} />
-        <Skeleton variant="text" width={280} height={42} />
-        <Skeleton variant="text" width={200} height={42} />
-        <Skeleton variant="text" width={160} height={42} />
-
-        {/* Button */}
-        <Skeleton variant="rectangular" width={140} height={38} />
-      </Stack>
-    </Box>
+      {/* Button */}
+      <Skeleton variant="rectangular" width={140} height={38} />
+    </Stack>
   )
 }
