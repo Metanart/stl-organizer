465c969f4763f7c1f3e6b79c816551efff41e5a3
Ilya Katassonov <ilya.katassonov@gmail.com>
2025-06-13 15:08:36 +0100

    Removed Sources from AppDataSource

--- src/main/database/AppDataSource.ts
+++ src/main/database/AppDataSource.ts
@@ -3,14 +3,13 @@ import path from 'path'
 import { DataSource } from 'typeorm'
 
 import { Config } from './models/common/Config'
-import { SourceFolder } from './models/common/Source'
 
 export const dbPath = path.join(app.getPath('userData'), 'stl-organizer.sqlite')
 
 export const AppDataSource = new DataSource({
   type: 'sqlite',
   database: dbPath,
-  entities: [Config, SourceFolder],
+  entities: [Config],
   synchronize: true, // 🔧 DEV FLAG
   logging: false,
   cache: false
