d8892e393cf0c0ee269a2e43a5e0ead11aa1c059
Ilya Katassonov <ilya.katassonov@gmail.com>
2025-07-19 16:22:08 +0100

    Installed storybook

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,3 +1,6 @@
+// For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
+import storybook from 'eslint-plugin-storybook'
+
 import tseslint from '@electron-toolkit/eslint-config-ts'
 import eslintConfigPrettier from '@electron-toolkit/eslint-config-prettier'
 import eslintPluginReact from 'eslint-plugin-react'
@@ -64,5 +67,17 @@ export default tseslint.config(
       ]
     }
   },
+
+  // Storybook rules
+  {
+    files: ['**/*.stories.@(ts|tsx)'],
+    plugins: {
+      storybook
+    },
+    rules: {
+      ...storybook.configs.recommended.rules
+    }
+  },
+
   eslintConfigPrettier
 )
