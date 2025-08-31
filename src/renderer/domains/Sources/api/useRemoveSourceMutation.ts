e85aac356f637cfcd5986d68aa029eb5bd7c3c05
Ilya Katassonov <ilya.katassonov@gmail.com>
2025-09-01 00:29:06 +0100

    Added useRemoveSourceMutation

---
+++ src/renderer/domains/Sources/api/useRemoveSourceMutation.ts
@@ -0,0 +1,48 @@
+import { RemoveDTO } from '@shared/domains/Common/Common.dtos'
+import { ApiDomain, ApiMethod } from '@shared/domains/Common/types/Api.types'
+import { createLog } from '@shared/utils/createLog'
+
+import { SourcesApi, SourcesApiTags } from './SourcesApi'
+
+type QueryReturn = {
+  domain: ApiDomain
+  method: ApiMethod
+  payload: RemoveDTO
+}
+
+function query(removeDto: RemoveDTO): QueryReturn {
+  const log = createLog({ tag: 'Sources.remove', category: 'RENDERER' })
+
+  log.info('Taking remove dto', removeDto)
+
+  return {
+    domain: 'Sources',
+    method: 'remove',
+    payload: removeDto
+  }
+}
+
+function transformResponse(result: boolean): boolean {
+  const log = createLog({ tag: 'Sources.remove', category: 'RENDERER' })
+
+  log.success('Removal result', result)
+
+  return result
+}
+
+function invalidatesTags(result, error): SourcesApiTags[] {
+  if (error || !result) return []
+
+  return ['Sources']
+}
+
+export const { useRemoveSourceMutation } = SourcesApi.injectEndpoints({
+  endpoints: (builder) => ({
+    removeSource: builder.mutation<boolean, RemoveDTO>({
+      query,
+      transformResponse,
+      invalidatesTags
+    })
+  }),
+  overrideExisting: false
+})
